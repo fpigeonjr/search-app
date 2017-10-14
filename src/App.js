515a9a8c18bc55458a485a197f950b3f2bddb825
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-10-13 23:55:38 -0500

    adds React-Bootstrap and adds SearchForm and Results components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Jumbotron, Grid } from "react-bootstrap";
+import SearchForm from "./components/SeachForm";
+import Results from "./components/Results";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Search App</h1>
+            <p>This is a simple search app.</p>
+            <SearchForm />
+          </Grid>
+        </Jumbotron>
+        <Results />
       </div>
     );
   }
